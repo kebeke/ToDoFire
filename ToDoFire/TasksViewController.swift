010575186a78876577f3d392ca59d39b94e1d4ec
Angelina <kebeke@list.ru>
2022-03-22 10:32:18 +0100

    Commit 2

---
+++ ToDoFire/TasksViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TasksViewController.swift
+//  ToDoFire
+//
+//  Created by Johnny Bravo on 22/03/2022.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
