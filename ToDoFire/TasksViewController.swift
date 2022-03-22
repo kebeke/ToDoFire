ddd6b1db1cc59de10eb6368dafea4bcd18291b93
Angelina <kebeke@list.ru>
2022-03-22 19:36:57 +0100

    Commit 3

--- ToDoFire/TasksViewController.swift
+++ ToDoFire/TasksViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var TableView: UITableView!
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
@@ -21,7 +22,12 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
     
+
+
 
     /*
     // MARK: - Navigation
